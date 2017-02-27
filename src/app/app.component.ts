63a6089185d798a86e6e2546fdba5cbeb21b52d4
APRxFE <aprxfe@gmail.com>
2017-02-27 15:09:15 +0900

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import * as Rx from 'rxjs'
+
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
 
@@ -8,7 +10,23 @@ import { FireCorx } from '../../ngModules/FireCorx'
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+	valueSt: any = new Rx.Subject()
+
+	_value: string = ''
+	get value(){ return this._value }
+	set value(val){ this.valueSt.next(val) }
+	value$ = this.valueSt.subscribe(d => {
+		console.log(d)
+		this._value = d
+	})
+
 	constructor(
 		public fireCorx: FireCorx
-	){}	
+	){
+
+		this.value = 'core'
+		this.value = 'core2'
+
+	}	
 }
