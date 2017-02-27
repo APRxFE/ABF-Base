04cea1a5396770e26ef3514398719ce90b2d0363
APRxFE <aprxfe@gmail.com>
2017-02-27 15:57:07 +0900

    update to FireCorx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,9 @@ import { FireCorx } from '../../ngModules/FireCorx'
 })
 export class AppComponent {
 
-	valueSt: any = new Rx.Subject()
-
-	_value: string = ''
-	get value(){ return this._value }
-	set value(val){ this.valueSt.next(val) }
-	value$ = this.valueSt.subscribe(d => {
-		console.log(d)
-		this._value = d
-	})
-
 	constructor(
 		public fireCorx: FireCorx
 	){
-
-		this.value = 'core'
-		this.value = 'core2'
 
 	}	
 }
